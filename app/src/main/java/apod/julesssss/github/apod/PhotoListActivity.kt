cd9c43e3e23e29c4102a230d2fda513900e54600
Jules <julianrosser91@gmail.com>
2018-12-27 20:11:26 +0000

    Create Android Studio project

---
+++ app/src/main/java/apod/julesssss/github/apod/PhotoListActivity.kt
@@ -0,0 +1,12 @@
+package apod.julesssss.github.apod
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class PhotoListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_list)
+    }
+}
