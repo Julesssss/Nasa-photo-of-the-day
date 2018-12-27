87eed58ba82269729c824cf53c68fc558375d1cc
Jules <julianrosser91@gmail.com>
2018-12-27 21:27:45 +0000

    Migrate to AndroidX and refactor dependency structure

--- app/src/main/java/apod/julesssss/github/apod/PhotoListActivity.kt
+++ app/src/main/java/apod/julesssss/github/apod/PhotoListActivity.kt
@@ -1,7 +1,7 @@
 package apod.julesssss.github.apod
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 class PhotoListActivity : AppCompatActivity() {
 
