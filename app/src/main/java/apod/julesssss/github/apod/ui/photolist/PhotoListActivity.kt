34661b1dd08e3cdc3ffa8f2eef28c08439f5a586
Jules <julianrosser91@gmail.com>
2018-12-27 22:21:54 +0000

    Create ui and photolist package

--- app/src/main/java/apod/julesssss/github/apod/ui/photolist/PhotoListActivity.kt
+++ app/src/main/java/apod/julesssss/github/apod/ui/photolist/PhotoListActivity.kt
@@ -1,12 +1,18 @@
-package apod.julesssss.github.apod
+package apod.julesssss.github.apod.ui.photolist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import apod.julesssss.github.apod.R
+import kotlinx.android.synthetic.main.activity_photo_list.*
 
 class PhotoListActivity : AppCompatActivity() {
+
+    private val viewModel by lazy { PhotoListViewModel() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_list)
+
+        photoListTextTemp.text = viewModel.getPhotoList().toString()
     }
 }
---
+++ app/src/main/java/apod/julesssss/github/apod/ui/photolist/PhotoListViewModel.kt
@@ -0,0 +1,6 @@
+package apod.julesssss.github.apod.ui.photolist
+
+class PhotoListViewModel {
+
+    fun getPhotoList() = listOf("PhotoA", "photoB", "photoC")
+}
