669364c40c93b74f009098491b72014577f047b4
Jules <julianrosser91@gmail.com>
2018-12-28 13:06:08 +0000

    Create basic progress and error message Views

--- app/src/main/java/apod/julesssss/github/apod/ui/photolist/PhotoListActivity.kt
+++ app/src/main/java/apod/julesssss/github/apod/ui/photolist/PhotoListActivity.kt
@@ -1,11 +1,12 @@
 package apod.julesssss.github.apod.ui.photolist
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import apod.julesssss.github.apod.R
 import apod.julesssss.github.apod.extension.findViewModel
-import apod.julesssss.github.apod.extension.toast
+import kotlinx.android.synthetic.main.activity_photo_list.*
 
 class PhotoListActivity : AppCompatActivity() {
 
@@ -24,6 +25,8 @@ class PhotoListActivity : AppCompatActivity() {
     }
 
     private fun updateState(state: PhotoListViewModel.ViewState) {
-        toast(state.data.toString())
+        photoListTextTemp.text = "data: \n\n${state.data.map { "- ${it.imageUrl} \n\n" }}"
+        photoListProgressBar.visibility = if (state.isLoading) View.VISIBLE else View.GONE
+        photoListTextError.text = state.errorMessage
     }
 }
